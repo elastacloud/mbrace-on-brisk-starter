f288a7e0d70e3e5e69a6ddec62209585f153fb6a
npal <nick.palladinos@gmail.com>
2015-02-19 17:48:17 +0200

    Cloud parallel url-downloader and cloudfiles read/write

--- src/Demos/mbrace.fsx
+++ src/Demos/mbrace.fsx
@@ -7,11 +7,12 @@ open MBrace.Azure.Runtime
 open System.Threading
 open System
 
-// Connect to mbrace
+ Connect to mbrace
 let config = 
     { Configuration.Default with
         StorageConnectionString = "put your storage connection string here"
         ServiceBusConnectionString = "put your service bus connection string here" }
+
 
 let runtime = Runtime.GetHandle(config)
 
@@ -58,3 +59,50 @@ open MBrace.Streams
 |> CloudStream.countBy id
 |> CloudStream.toArray
 |> runtime.Run
+
+
+
+// Cloud parallel url-downloader
+open System.IO
+open System.Net
+open MBrace.Streams
+
+let urls = [| ("bing", "http://bing.com"); ("yahoo", "http://yahoo.com"); 
+              ("google", "http://google.com"); ("msn", "http://msn.com") |]
+
+let write (text: string) (stream: Stream) = async { 
+    use writer = new StreamWriter(stream)
+    writer.Write(text)
+    return () 
+}
+
+
+let download (name: string, uri: string) = cloud {
+    let webClient = new WebClient()
+    let! text = Cloud.OfAsync <| webClient.AsyncDownloadString(Uri(uri))
+    let! file = CloudFile.New(write text, sprintf "pages/%s.html" name)
+    return file
+}
+
+let proc = 
+    urls 
+    |> Array.map download
+    |> Cloud.Parallel
+    |> runtime.CreateProcess
+
+let files = proc.AwaitResult()
+
+
+let read (file: MBrace.CloudFile) = cloud {
+    let! text = CloudFile.Read(file, CloudFile.ReadAllText)
+    return (file.FileName, text.Length)
+}
+
+let proc' = 
+    files
+    |> Array.map read
+    |> Cloud.Parallel
+    |> runtime.CreateProcess
+
+let filesizes = proc'.AwaitResult()
+
